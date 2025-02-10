67eac71f1d56f56eb67d2375d4b075a2a302e5bf
Rustem Amirkhanuly <rekewka@Zholdassovas-MacBook-Air.local>
2025-02-10 21:13:17 +0500

    first commit

---
+++ lesson1.dart
@@ -0,0 +1,12 @@
+void main(){
+  print("Hello world!");
+}
+/*
+void does not return anything
+print is for printing message
+() -> function arguments
+{} is code zone, as : in python or as in c++ {}, java
+
+to run code write in terminal "dart run filename"
+for this example I need to write "dart run lesson1.dart"
+*/
---
+++ lesson2.dart
@@ -0,0 +1,25 @@
+void main(){
+
+  int a = 5;
+  var s = "string";
+  
+  print(a);
+  print(s);
+}
+
+/*
+variables is like a box, where you store some element. it allocates memory
+as in math, we call a = 5, same logic goes for language programming like python, c++ and also dart 
+
+keyword for creating variable is same as in javascript : var;
+also to create variables with data type same goes as in c++: int a = 5;
+
+'' and "" same as in python for dart
+
+when we define the type of data, we can't insert data which differs from main type
+
+for example : 
+int a = 5;
+a = "asdasdasd"; -> incorrect
+String, bool, double, int;
+*/
